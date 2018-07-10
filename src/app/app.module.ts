c35443fc725d7f5f41604f37bf1f58dbaefb22d4
Edison Rojas <edison.rojas@youse.com.co>
2018-07-10 12:31:49 -0500

    Create structure initial  project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardContactComponent } from './components/card-contact/card-contact.component';
+import { DetailsContactComponent } from './components/details-contact/details-contact.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CardContactComponent,
+    DetailsContactComponent,
+    FooterComponent,
+    ContactsComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/details-contact/details-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-contact',
+  templateUrl: './details-contact.component.html',
+  styleUrls: ['./details-contact.component.css']
+})
+export class DetailsContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
